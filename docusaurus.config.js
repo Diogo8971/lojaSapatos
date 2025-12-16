93bfaacb98875ea7d546ee93c640dd6ebfe76616
Tomás Marcelino <a6689@aeprosa.pt>
2025-12-16 22:17:48 +0000

    Atulizei o link do GitHub

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,7 +98,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Diogo8971/lojaSapatos',
             label: 'GitHub',
             position: 'right',
           },
